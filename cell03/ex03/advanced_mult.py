077f8ea1c499ad2f3267e6293feeb4f333d26ee3
trammer1 <trammer321@gmail.com>
2025-06-26 07:44:21 +0000

    Add advanced_mult.py to generate multiplication tables from 0 to 10

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,8 @@
+num = 0
+while num <= 10:
+    print(f"Table de {num}:", end=" ")
+    start = 0
+    while start <= 10:
+        print(f"{num * start}", end="\n" if start == 10 else " ")
+        start += 1
+    num += 1
