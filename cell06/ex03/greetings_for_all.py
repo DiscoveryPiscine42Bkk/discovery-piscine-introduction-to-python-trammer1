7174352bf4b487d8e11ca1fcc0bd6d70fd134ae2
trammer1 <trammer321@gmail.com>
2025-06-27 08:44:31 +0000

    Add upcase_it.py and downcase_all.py for string case manipulation; include greetings_for_all.py for personalized greetings

---
+++ cell06/ex03/greetings_for_all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def greetings(person = "Noble Stranger"):
+    try:
+        int(person)
+        print("Error! It was not a name.")
+    except:
+        print(f"Hello, {person}!")
+
+greetings('Alexandra')
+greetings('Wil')
+greetings()
+greetings(42)
